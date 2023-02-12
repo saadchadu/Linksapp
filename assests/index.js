0111ebcc9ac4407f8074ceaf51b650c82a15eb0c
saadchadu <saadchadu@gmail.com>
2023-02-12 21:42:41 +0530

    design fixes

--- assests/index.js
+++ assests/index.js
@@ -10,11 +10,8 @@ function generateLink() {
     let message = textBox.value;
     let url = "https://api.whatsapp.com/send?phone=";
     let end_url = `${url}${number}&text=${message}`;
-    console.log(end_url);
     
     navigator.clipboard.writeText(end_url).then(() => {
-        // Alert the user that the action took place.
-        // Nobody likes hidden stuff being done under the hood!
         alert("Copied to clipboard");
     });
 }
