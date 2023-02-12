6a3445137bf45364407dda1e547fb03d744e5526
saadchadu <saadchadu@gmail.com>
2023-02-12 20:50:54 +0530

    Favicon and javascript code done

--- assests/index.js
+++ assests/index.js
@@ -2,17 +2,19 @@ let phoneNumberInput = document.getElementById("phoneNumber");
 
 let textBox = document.querySelector("textarea");
 
-
 let generateBtn = document.getElementById("generate-btn");
 let copyBtn = document.getElementById("copy-btn");
 
-function generateWhatsAppLink(phoneNumber, text) {
-    return `https://wa.me/${phoneNumber}?text=${encodeURIComponent(text)}`;
+function generateLink() {
+    let number = phoneNumberInput.value;
+    let message = textBox.value;
+    let url = "https://api.whatsapp.com/send?phone=";
+    let end_url = `${url}${number}&text=${message}`;
+    console.log(end_url);
+    
+    navigator.clipboard.writeText(end_url).then(() => {
+        // Alert the user that the action took place.
+        // Nobody likes hidden stuff being done under the hood!
+        alert("Copied to clipboard");
+    });
 }
-
-var phoneNumber = phoneNumberInput.innerHTML;
-var text = "Hey there! How can I help you?";
-
-var whatsAppLink = generateWhatsAppLink(phoneNumber, text);
-console.log(whatsAppLink);
-console.log(phoneNumberInput.value);
