020def74c3f38e28cdeeb0597ba1d317074cd466
saadchadu <saadchadu@gmail.com>
2023-02-16 16:27:18 +0530

    Error on blacnk button click, other bug fixes

--- assests/index.js
+++ assests/index.js
@@ -3,10 +3,12 @@ let phoneNumberInput = document.getElementById("phoneNumber");
 let textBox = document.querySelector("textarea");
 
 let generateBtn = document.getElementById("generate-btn");
-let prevBtn = document.getElementById("preview-btn")
+let prevBtn = document.getElementById("preview-btn");
 let copyBtn = document.getElementById("copy-btn");
 
 let link = document.getElementById("link");
+let number = phoneNumberInput.value;
+let message = textBox.value;
 
 
 function generateLink() {
@@ -18,9 +20,20 @@ function generateLink() {
     end_url = removeSpace;
     link.innerHTML = end_url;
 
+    //
+    if(number==0){
+        function shakeTimout(){
+            phoneNumberInput.classList.add("shake");
+            link.innerText = " ";
+        }
+        shakeTimout();
+        setTimeout(() => {
+            phoneNumberInput.classList.remove('shake');
+            }, 1200); 
+    }
 }
 
-function previewLink(){
+function previewLink() {
     let number = phoneNumberInput.value;
     let message = textBox.value;
     let url = "https://api.whatsapp.com/send?phone=";
@@ -28,14 +41,23 @@ function previewLink(){
     let removeSpace = end_url.replace(/ /g, "%20");
     end_url = removeSpace;
     window.open(end_url);
-    
+
+    //
+    if(number==0){
+        function shakeTimout(){
+            phoneNumberInput.classList.add("shake");
+            link.innerText = " ";
+        }
+        shakeTimout();
+        setTimeout(() => {
+            phoneNumberInput.classList.remove('shake');
+            }, 1200); 
+    }
 }
 
-// const button = document.querySelector('button');
-// const url = 'https://example.com'; // replace with your desired URL
 
 
-function copyLink(){
+function copyLink() {
     let number = phoneNumberInput.value;
     let message = textBox.value;
     let url = "https://api.whatsapp.com/send?phone=";
@@ -43,9 +65,14 @@ function copyLink(){
     let removeSpace = end_url.replace(/ /g, "%20");
     end_url = removeSpace;
     navigator.clipboard.writeText(end_url).then(() => {
-        alert("Copied to clipboard");
+        var x = document.getElementById("snackbar");
+    x.className = "show";
+    setTimeout(function () {
+        x.className = x.className.replace("show", "");
+    }, 3000);
     });
+
+    
+    
 }
-
-
 
